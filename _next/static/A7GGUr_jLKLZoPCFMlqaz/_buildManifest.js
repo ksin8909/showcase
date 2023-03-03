5b2e7260d45ae52947539cb7943af58a415abe26
unknown <ksin8909@gmail.com>
2023-03-03 11:12:19 +0800

    Deploy

--- _next/static/A7GGUr_jLKLZoPCFMlqaz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-903bd488ebffb96d.js","static/chunks/29-a7273cd770dcbcd2.js","static/chunks/pages/index-567be31704a64838.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
